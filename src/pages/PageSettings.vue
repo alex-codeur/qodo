9082339f750cb4d37aa8fbf0d18e2ff6a16098b3
alex <apachecordovax@gmail.com>
2021-07-29 10:37:06 +0200

    initial commit

---
+++ src/pages/PageSettings.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <p>Settings Page</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
